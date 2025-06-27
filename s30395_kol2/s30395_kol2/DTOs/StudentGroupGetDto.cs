f3303d33f6fef3c08efa5748cc2ddc60e6aa04d4
Aleksandra <133956347+alessandra3747@users.noreply.github.com>
2025-06-27 16:18:49 +0200

    Delete kol2 directory

--- s30395_kol2/s30395_kol2/DTOs/StudentGroupGetDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace s30395_kol2.DTOs;
-
-public class StudentGroupGetDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Level { get; set; }
-    public DateTime AcademicYear { get; set; }
-}
--- s30395_kol2/s30395_kol2/Exceptions/NotFoundException.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace s30395_kol2.Exceptions;
-
-public class NotFoundException(string message) : Exception(message);
