6f16d08133643ca0fd2624d1d33910e5196e272e
Aleksandra <133956347+alessandra3747@users.noreply.github.com>
2025-06-27 16:31:06 +0200

    Delete PrescriptionApplication directory

--- PrescriptionApplication/PrescriptionApplication/Exceptions/NotFoundException.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace PrescriptionApplication.Exceptions;
-
-public class NotFoundException(string message) : Exception(message);
--- PrescriptionApplication/PrescriptionApplication/Exceptions/ValidationException.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PrescriptionApplication.Exceptions;
-
-public class ValidationException : Exception
-{
-    public ValidationException(string message) : base(message) { }
-}
